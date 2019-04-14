761ebb9cc9868046dbd385461fe5c5e07ad57700
Alberto Martinez <amartint@mercadona.es>
2019-04-14 13:44:05 +0200

    Rename to singular

--- src/api/book/book.controller.ts
+++ src/api/book/book.controller.ts
@@ -7,36 +7,36 @@ import { BookService } from './book.service';
 @ApiUseTags('books')
 @Controller('books')
 export class BookController {
-  constructor(private booksService: BookService) {}
+  constructor(private readonly bookService: BookService) {}
 
   @Get()
   public async getBooks() {
-    const books = await this.booksService.getBooks();
+    const books = await this.bookService.getBooks();
     return books;
   }
 
   @Get(':bookId')
   public async getBook(@Param('bookId') bookId: string) {
-    const book = await this.booksService.getBook(bookId);
+    const book = await this.bookService.getBook(bookId);
     return book;
   }
 
   @Post()
   public async addBook(@Body() book: CreateBookDto) {
     // , @SessionUser() user: UserEntity
-    const createdBook = await this.booksService.addBook(book);
+    const createdBook = await this.bookService.addBook(book);
     return createdBook;
   }
 
   @Put(':bookId')
   public async updateBook(@Param('bookId') bookId: string, @Body() book: CreateBookDto) {
-    const createdBook = await this.booksService.updateBook(bookId, book);
+    const createdBook = await this.bookService.updateBook(bookId, book);
     return createdBook;
   }
 
   @Delete()
   public async deleteBook(@Query() { bookId }: DeleteBookQuery) {
-    const books = await this.booksService.deleteBook(bookId);
+    const books = await this.bookService.deleteBook(bookId);
     return books;
   }
 }
